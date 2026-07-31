306b0b442a9f35a6ef80b6c488deabfa08c57907
Athika Priya <athikapriya1997@gmail.com>
2026-07-31 09:01:59 +0600

    Solve LeetCode 0496 (Easy) : Next Greater Element I

---
+++ Monotonic Stack/0496. Next Greater Element I.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        next_greater = {}
+
+        for num in nums2:
+            while stack and num > stack[-1]:
+                next_greater[stack.pop()] = num
+            stack.append(num)
+
+        while stack:
+            next_greater[stack.pop()] = -1
+
+        result = []
+
+        for num in nums1:
+            result.append(next_greater[num])
+
+        return result
+
+"""
+Time Complexity : O(n + m)
+Space Complexity : O(n + m)
+"""
--- README.md
+++ README.md
@@ -20,6 +20,7 @@ This repository documents my journey of improving problem-solving skills, learni
 | 0242 | Valid Anagram | Hash Table, String | Easy |
 | 0392 | Is Subsequence | Two Pinters, String, Dynamic Programming | Easy |
 | 0485 | Max Consecutive Ones | Array | Easy |
+| 0496 | Next Greater Element I | Monotonic Stack, Stack, Hash Table, Array | Easy |
 | 0605 | Can place Flowers | Greedy, Array | Easy |
 | 0628 | Maximum Product of Three Numbers | Sorting, Array, Math | Easy |
 | 0929 | Unique Email Address | Hash Table, String | Easy |
