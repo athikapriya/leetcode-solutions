b756fc5e31600ff7619b71c0c0783dac003513f5
Athika Priya <athikapriya1997@gmail.com>
2026-08-10 10:42:25 +0600

    Solve Leetcode 1512(Easy) : Number of Good Pairs

---
+++ Hash Table/1512. Number of Good Pairs.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = {}
+        pairs = 0
+
+        for num in nums:
+            pairs += count.get(num, 0)
+            count[num] = count.get(num, 0) + 1
+            
+        return pairs
+
+
+"""
+Time Complexity : O(n)
+Space complexity : O(n)
+"""
